993d3a3fbd28441508ab67e34dc68116fe6eca4f
doantruc1989 <doantruc1989@gmail.com>
2023-04-25 17:00:17 +0700

    fix fe

--- wp-content/plugins/newPlugin.php
+++ wp-content/plugins/newPlugin.php
@@ -7,77 +7,115 @@ add_shortcode('thuonghieunoibat', 'create_thuonghieunoibat');
 function create_thuonghieunoibat()
 {
     return "
-    <div class='grid grid-cols-4 md:grid-cols-6 lg:grid-cols-9 gap-2 items-center justify-center mt-10'>
+    <div class='grid grid-cols-4 md:grid-cols-6 lg:grid-cols-9 gap-2 items-center justify-center mt-6'>
     <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/9wishes.png' alt=''></a>
+href='http://localhost/wordpress/brand/9wishes/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/9wishes.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/anessa.png' alt=''></a>
+href='http://localhost/wordpress/brand/anessa/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/anessa.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/bioderma.png' alt=''></a>
+href='http://localhost/wordpress/brand/bioderma/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/bioderma.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/biore.png' alt=''></a>
+href='http://localhost/wordpress/brand/biore/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/biore.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/blackrouge.png' alt=''></a>
+href='http://localhost/wordpress/brand/blackrouge/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/blackrouge.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/caryophy.png' alt=''></a>
+href='http://localhost/wordpress/brand/caryophy/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/caryophy.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/cellfusion.png' alt=''></a>
+href='http://localhost/wordpress/brand/cellfusionc/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/cellfusion.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/cerave.png' alt=''></a>
+href='http://localhost/wordpress/brand/cerave/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/cerave.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/cetaphil.png' alt=''></a>
+href='http://localhost/wordpress/brand/cetaphil/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/cetaphil.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/cocoon.png' alt=''></a>
+href='http://localhost/wordpress/brand/cocoon/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/cocoon.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/cosrx.png' alt=''></a>
+href='http://localhost/wordpress/brand/cosrx/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/cosrx.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/dhc.png' alt=''></a>
+href='http://localhost/wordpress/brand/dhc/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/dhc.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/hatomugi.png' alt=''></a>
+href='http://localhost/wordpress/brand/hatomugi/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/hatomugi.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/huxley.png' alt=''></a>
+href='http://localhost/wordpress/brand/huxley/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/huxley.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/imfrom.png' alt=''></a>
+href='http://localhost/wordpress/brand/imfrom/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/imfrom.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/innisfree.png' alt=''></a>
+href='http://localhost/wordpress/brand/innisfree/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/innisfree.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/klairs.png' alt=''></a>
+href='http://localhost/wordpress/brand/klairs/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/klairs.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/kose.png' alt=''></a>
+href='http://localhost/wordpress/brand/kose/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/kose.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/larocheposay.png' alt=''></a>
+href='http://localhost/wordpress/brand/larocheposay/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/larocheposay.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/loreal.png' alt=''></a>
+href='http://localhost/wordpress/brand/loreal/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/loreal.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/mise-en.png' alt=''></a>
+href='http://localhost/wordpress/brand/mise-en/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/mise-en.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/naruko.png' alt=''></a>
+href='http://localhost/wordpress/brand/naruko/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/naruko.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/neutrogena.png' alt=''></a>
+href='http://localhost/wordpress/brand/neutrogena/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/neutrogena.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/obagi.png' alt=''></a>
+href='http://localhost/wordpress/brand/obagi/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/obagi.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/romend.png' alt=''></a>
+href='http://localhost/wordpress/brand/romend/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/romend.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/senka.png' alt=''></a>
+href='http://localhost/wordpress/brand/senka/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/senka.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/simple.png' alt=''></a>
+href='http://localhost/wordpress/brand/simple/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/simple.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/sivanna.png' alt=''></a>
+href='http://localhost/wordpress/brand/sivanna/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/sivanna.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/skin1004.png' alt=''></a>
+href='http://localhost/wordpress/brand/skin1004/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/skin1004.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/somebymi.png' alt=''></a>
+href='http://localhost/wordpress/brand/somebymi/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/somebymi.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/stives.png' alt=''></a>
+href='http://localhost/wordpress/brand/stives/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/stives.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/theordinary.png' alt=''></a>
+href='http://localhost/wordpress/brand/theordinary/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/theordinary.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/unicharm.png' alt=''></a>
+href='http://localhost/wordpress/brand/unicharm/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/unicharm.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/vaseline.png' alt=''></a>
+href='http://localhost/wordpress/brand/vaseline/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/vaseline.png' alt=''></a>
 <a class='border border-gray-300 hover:shadow-md shadow-sm flex items-center justify-self-center'
-href=''><img class='w-20 h-20' src='http://localhost/wordpress/wp-content/uploads/brand/whiteconc.png' alt=''></a>
+href='http://localhost/wordpress/brand/whiteconc/'><img class='w-28 h-28' src='http://localhost/wordpress/wp-content/uploads/brand/whiteconc.png' alt=''></a>
     </div>
     ";
+}
+
+add_shortcode('fourbanner', 'create_banner');
+function create_banner() {
+    return "
+    <div class='w-full grid lg:grid-cols-2 gap-2'>
+   
+        <a href='http://localhost/wordpress/danh-muc-san-pham/cham-soc-da-mat/'>
+            <img class='w-full rounded-lg' src='http://localhost/wordpress/wp-content/uploads/banner/banner1.png' alt=''>
+        </a>
+
+
+        <a href='http://localhost/wordpress/danh-muc-san-pham/gia-tot-hom-nay/'>
+            <img class='w-full rounded-lg' src='http://localhost/wordpress/wp-content/uploads/banner/banner2.png' alt=''>
+        </a>
+
+        <a href='http://localhost/wordpress/danh-muc-san-pham/cham-soc-co-the/'>
+            <img class='w-full rounded-lg' src='http://localhost/wordpress/wp-content/uploads/banner/banner3.png' alt=''>
+        </a>
+
+        <a href='http://localhost/wordpress/danh-muc-san-pham/trang-diem/'>
+            <img class='w-full rounded-lg' src='http://localhost/wordpress/wp-content/uploads/banner/banner4.png' alt=''>
+        </a>
+    </div>
+
+    ";
+}
+
+add_shortcode('topbanner', 'create_topbanner');
+function create_topbanner() {
+    return "
+    <div class='w-full'>
+   
+        <a href='http://localhost/wordpress/danh-muc-san-pham/gia-tot-hom-nay/'>
+            <img class='w-full' src='http://localhost/wordpress/wp-content/uploads/banner/banner5.png' alt=''>
+        </a>
+        </div>
+        ";
 }
--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -98,7 +98,123 @@ function my_text_strings($translated_text, $text, $domain)
 			$translated_text = __('Sản phẩm liên quan', 'woocommerce');
 			break;
 	}
+    switch ($translated_text) {
+		case 'View more':
+			$translated_text = __('Xem thêm', 'woocommerce');
+			break;
+	}
 	return $translated_text;
 }
 add_filter('gettext', 'my_text_strings', 20, 3);
 
+// Register Product Brand Taxonomy
+function tb_product_brand_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Thương hiệu', 'Taxonomy General Name', 'tb_product_brand' ),
+		'singular_name'              => _x( 'Product Brand', 'Taxonomy Singular Name', 'tb_product_brand' ),
+		'menu_name'                  => __( 'Product Brands', 'tb_product_brand' ),
+		'all_items'                  => __( 'All Brands', 'tb_product_brand' ),
+		'parent_item'                => __( 'Parent Brand', 'tb_product_brand' ),
+		'parent_item_colon'          => __( 'Parent Brand:', 'tb_product_brand' ),
+		'new_item_name'              => __( 'New Item Brand', 'tb_product_brand' ),
+		'add_new_item'               => __( 'Add New Brand', 'tb_product_brand' ),
+		'edit_item'                  => __( 'Edit Brand', 'tb_product_brand' ),
+		'update_item'                => __( 'Update Brand', 'tb_product_brand' ),
+		'view_item'                  => __( 'View Brand', 'tb_product_brand' ),
+		'separate_items_with_commas' => __( 'Separate brands with commas', 'tb_product_brand' ),
+		'add_or_remove_items'        => __( 'Add or remove brands', 'tb_product_brand' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'tb_product_brand' ),
+		'popular_items'              => __( 'Popular Brands', 'tb_product_brand' ),
+		'search_items'               => __( 'Search Brand', 'tb_product_brand' ),
+		'not_found'                  => __( 'Not Found', 'tb_product_brand' ),
+		'no_terms'                   => __( 'No brands', 'tb_product_brand' ),
+		'items_list'                 => __( 'Brandlist', 'tb_product_brand' ),
+		'items_list_navigation'      => __( 'Brand list navigation', 'tb_product_brand' ),
+	);
+	$rewrite = array(
+		'slug'                       => 'brand',
+		'with_front'                 => true,
+		'hierarchical'               => true,
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+		'rewrite'                    => $rewrite,
+		'show_in_rest'               => true,
+	);
+	register_taxonomy( 'tb_product_brand', array( 'product' ), $args );
+
+}
+add_action( 'init', 'tb_product_brand_taxonomy', 0 );
+
+// Add ACF thumbnail columns to Product Brand custom taxonomy
+
+function add_thumbnail_columns($columns) {
+   $columns['brand_logo'] = __('Logo');
+   $new = array();
+   foreach ($columns as $key => $value) {
+       if ($key=='name') // Put the Thumbnail column before the Name column
+           $new['brand_logo'] = 'Logo';
+       $new[$key] = $value;
+   }
+   return $new;
+   // Code end
+}
+add_filter('manage_edit-tb_product_brand_columns', 'add_thumbnail_columns');
+
+/**
+* Output ACF thumbnail content in Linen Category custom taxonomy columns
+*/
+function thumbnail_columns_content($content, $column_name, $term_id) {
+   if ('brand_logo' == $column_name) {
+       $term = get_term($term_id);
+      // $linen_thumbnail_var = get_field('thumbnail_id', $term);
+      
+      $image_id = get_term_meta( $term->term_id, 'brand_logo', true );
+      $image_data = wp_get_attachment_image_src( $image_id, 'full' );
+     
+      if ( $image_data != false ) {
+          $image = $image_data[0];
+         $content = '<img src="' .  esc_url( $image ) . '" width = "60px" alt="'. $term->name .'"/>';
+      }
+     
+      /*if ( $image_id ) {
+           $image = wp_get_attachment_image( $image_id,  array('80', 'auto') );
+           if( $image ){
+               $content = $image;
+           }
+         }*/		
+     
+      //$content = '<img src="'.$linen_thumbnail_var['url'].'" width="60" />';
+       //$content = print_r($image_data);
+    }
+   return $content;
+}
+
+// Add Brand Logo to Single Product page  and Product Archive
+// add_action('woocommerce_single_product_summary','tb_single_product_logo', 1);
+// add_action('woocommerce_after_shop_loop_item', 'tb_single_product_logo', 10 );
+// function tb_single_product_logo() {
+  
+//    $terms = get_the_terms( get_the_ID(), 'tb_product_brand');
+//   //print_r($terms);
+//   if(is_array($terms) || is_object($terms)){
+// 	echo '<div class="tb-brand-logo">';
+//   	foreach ((array) $terms as $term ) {
+//     	$thumbnail_id = get_term_meta($term->term_id, 'brand_logo', true ) ;
+// 	  	if ( $thumbnail_id ) {
+// 			$image = wp_get_attachment_image( $thumbnail_id, 'full', $icon = false ,  array( 'class' => 'brand-logo', 'alt' => $term->name ) );
+// 			if( $image ){
+// 				echo sprintf( '<a href="%s">%s</a>', get_term_link( $term ), $image );
+// 			}
+//   		}		
+//   	}
+// 	echo '</div>';
+//   }
+// }
